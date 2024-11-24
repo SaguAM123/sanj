10e86dda7fad0fd6a3b52b8adff69bdcbc70dcc4
SaguAM123 <166099007+SaguAM123@users.noreply.github.com>
2024-11-24 10:28:29 +0530

    Add files via upload

---
+++ sagar.py
@@ -0,0 +1,21 @@
+name=input("EnterYour name:")
+EnterYour name:sagar
+sem=int(input("Enter Your Semester:"))
+Enter Your Semester:4
+city=input("Enter Your city name:")
+Enter Your city name:kanakapura
+pincode=int(input("Enter a pincode of Your city:"))
+Enter a pincode of Your city:562117
+print("\n\n\n")
+
+
+
+
+print("Student Name:",name)
+Student Name: sagar
+print("Semester:",sem)
+Semester: 4
+print("city:",city)
+city: kanakapura
+print("pincode:",pincode)
+pincode: 562117
